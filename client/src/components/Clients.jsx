e37e721ed71bbe9abed6fcf2399d9800e0020b6a
Oleg Glingeanu <oleg.glingeanu@gmail.com>
2022-10-04 19:22:16 +0100

    Working on client mutation
    setting up a delete client mutation which  uses apollo and gql to make a call to mongo to delete a client from the data base

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,27 +1,32 @@
-import { gql, useQuery } from '@apollo/client'
-
-const GET_CLIENTS = gql`
-    query getClients {
-        clients{
-          id
-          name
-          email
-          phone
-        }
-      }
-`
+import { useQuery } from '@apollo/client'
+import ClientRow from './ClientRow'
+import { GET_CLIENTS } from '../queries/clientQueries'
+import Spinner from './Spinner'
 
 export default function Clients() {
     const { loading, error, data} = useQuery(GET_CLIENTS)
 
-    if ( loading ) return <p>Loading..</p>
+    if ( loading ) return <Spinner />
     if( error ) return <p>Something went teribly wrong !</p>
 
 
     return (
         <>
             {!loading && !error && (
-                <h1>Clients</h1>
+                <table className='table table-hover mt-3'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.clients.map(client =>(
+                            <ClientRow key={client.id} client={client}/>
+                        ))}
+                    </tbody>
+                </table>
             )}
         </>
   )
